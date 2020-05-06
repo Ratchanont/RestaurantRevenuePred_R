0a38afbf16e16dd8adb11cbfdb607e1b11e2286e
Ratchanont <rruengsil@gmail.com>
2020-05-06 19:14:52 +0700

    More EDA, Plot, fitting models

--- main script.R
+++ main script.R
@@ -2,6 +2,9 @@
 
 library(caTools)
 library(dplyr)
+library(tidyr)
+library(purrr)
+library(ggplot2)
 
 #load data & first check
 
@@ -37,4 +40,89 @@ train_new$typeLabel <- if_else(train_new$Type == 'IL',1
                                ,if_else(train_new$Type == 'FC',2,3))
 
 head(train_new)
+
+str(train)
+
+train_new$year <- strtoi(substr(train_new$Open.Date,7,10))
+str(train_new)
+
+train_new$age <- 2020 - train_new$year
+str(train_new)
+
+drop <- c('City','Id','Open.Date','City.Group','Type','year')
+
+train_final <- train_new[,!names(train_new) %in% drop]
+
+head(train_final)
+
+#EDA v.2
+
+head(train_final)
+
+train_finalLong <- gather(train_final,key='measure',value='value',-revenue)
+train_finalLong
+unique(train_finalLong$measure)
+
+revenue <- train_final$revenue
+revenue_rep <- rep(revenue, 41)
+
+  #Try 1 plot
+ggplot(train_final,aes(x=P2,y=revenue))+
+  geom_point()+
+  ylim(0,1.0e+07)
+
+  #Facet plot
+ggplot(train_finalLong,aes(x=value,y=revenue_rep))+
+  geom_line()+
+  facet_wrap(~measure)+
+  ylim(0,1.0e+07)
+  
+#EDA v.3 Big/Other cities
+
+
+
+#Try fit model
+
+train_final_trim <- train_final[train_final$revenue<1.0e+07,]
+summary(train_final_trim)
+
+mod_test <- lm(revenue ~ .,data=train_final_trim)
+summary(mod_test)
+
+mod_test2 <-
+
+#Train model
+
+set.seed(101)
+sample = sample.split(train_final, SplitRatio = 0.7)
+head(sample)
+
+tr = subset(train_final, sample == TRUE)
+te = subset(train_final, sample == FALSE)
+
+mod <- lm(revenue ~ age,data = tr)
+summary(mod)
+
+mod1 <- lm(revenue ~ age+typeLabel,data = tr)
+summary(mod1)
+
+mod_final <- lm(revenue ~ age,data = tr)
+summary(mod_final)
+
+pred_final <- predict(mod_final,newdata = te)
+head(pred_final)
+
+SSE = sum((pred_final - te$revenue)^2)
+SST = sum((mean(te$revenue) - te$revenue)^2)
+R2 = 1-SSE/SST
+R2
+
+RMSE = sqrt(SSE/nrow(te))
+RMSE
+
+mod_final2 <- lm(revenue ~ age,data = train_final)
+
+#Predict on test data
+
+test_df <- read.csv('D:/Data science/Kaggle/RestaurantRevenuePredict/test.csv')
 
