e2dbe593e533d9803b5961675304a078e882ce7e
Ratchanont <rruengsil@gmail.com>
2020-05-06 20:14:11 +0700

    Final with csv generated

--- main script.R
+++ main script.R
@@ -79,7 +79,20 @@ ggplot(train_finalLong,aes(x=value,y=revenue_rep))+
   
 #EDA v.3 Big/Other cities
 
+train_BigCityLong <- gather(train_final[train_final$cityGroupLabel == 1,],key='measure',value='value',-revenue)
+str(train_BigCityLong)
 
+revenueBigCity <- train_final[train_final$cityGroupLabel == 1,'revenue']
+head(revenueBigCity)
+length(revenueBigCity)
+
+revenueBigCity_rep <- rep(revenueBigCity, 41)
+
+  #Facet plot
+ggplot(train_BigCityLong,aes(x=value,y=revenueBigCity_rep))+
+  geom_line()+
+  facet_wrap(~measure)+
+  ylim(0,1.0e+07)
 
 #Try fit model
 
@@ -89,25 +102,20 @@ summary(train_final_trim)
 mod_test <- lm(revenue ~ .,data=train_final_trim)
 summary(mod_test)
 
-mod_test2 <-
+mod_test2 <- lm(revenue ~ P20+P28,data=train_final_trim)
+summary(mod_test2)
 
 #Train model
 
 set.seed(101)
-sample = sample.split(train_final, SplitRatio = 0.7)
+sample = sample.split(train_final_trim, SplitRatio = 0.7)
 head(sample)
 
-tr = subset(train_final, sample == TRUE)
-te = subset(train_final, sample == FALSE)
+tr = subset(train_final_trim, sample == TRUE)
+te = subset(train_final_trim, sample == FALSE)
 
-mod <- lm(revenue ~ age,data = tr)
+mod_final <- lm(revenue ~ P20+P28,data=tr)
 summary(mod)
-
-mod1 <- lm(revenue ~ age+typeLabel,data = tr)
-summary(mod1)
-
-mod_final <- lm(revenue ~ age,data = tr)
-summary(mod_final)
 
 pred_final <- predict(mod_final,newdata = te)
 head(pred_final)
@@ -120,9 +128,23 @@ R2
 RMSE = sqrt(SSE/nrow(te))
 RMSE
 
-mod_final2 <- lm(revenue ~ age,data = train_final)
+mod_final2 <- lm(revenue ~ P20+P28,data = train_final)
 
 #Predict on test data
 
 test_df <- read.csv('D:/Data science/Kaggle/RestaurantRevenuePredict/test.csv')
 
+pred_test <- predict(mod_final2,newdata = test_df)
+
+length(pred_test)
+nrow(test_df)
+
+head(pred_test)
+
+Id <- test_df[,'Id']
+Prediction <- pred_test
+filename <- 'result.csv'
+
+result <- data.frame(Id,Prediction)
+head(result)
+write.csv(result,paste('D:/Data science/Kaggle/RestaurantRevenuePred_R/',filename),row.names = FALSE)
